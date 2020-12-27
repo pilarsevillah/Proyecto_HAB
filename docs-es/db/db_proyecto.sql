ed7ddbc8d85eb51c6c64e2e51e7c7c8b7f75b28c
jmfdezb <jmfernandezb@outlook.es>
2020-12-27 17:32:22 +0100

    Reordenación de tablas

--- docs-es/db/db_proyecto.sql
+++ docs-es/db/db_proyecto.sql
@@ -1,53 +1,6 @@
-CREATE DATABASE  IF NOT EXISTS `db_proyecto_consulta`;
+DROP DATABASE IF EXISTS `db_proyecto_consulta`;
+CREATE DATABASE IF NOT EXISTS `db_proyecto_consulta`;
 USE `db_proyecto_consulta`;
-
-
-DROP TABLE IF EXISTS `answer`;
-
-CREATE TABLE `answer` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `validated` tinyint(1) NOT NULL DEFAULT '0',
-  `validated_at` int unsigned DEFAULT NULL,
-  `validated_by_id` bigint DEFAULT NULL,
-  `content_id` bigint NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  KEY `fk_answer_1_idx` (`content_id`),
-  KEY `fk_answer_2_idx` (`validated_by_id`),
-  CONSTRAINT `fk_answer_1` FOREIGN KEY (`content_id`) REFERENCES `question_content` (`id`),
-  CONSTRAINT `fk_answer_2` FOREIGN KEY (`validated_by_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-
-
-DROP TABLE IF EXIST `avatar`;
-
-CREATE TABLE `avatar` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `user_id` bigint NOT NULL,
-  `type` enum('gravatar','custom') NOT NULL DEFAULT 'custom',
-  `avatar` varchar(1024) NOT NULL,
-  `selected` tinyint(1) NOT NULL DEFAULT '1',
-  `added_at` int unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  KEY `fk_avatar_1_idx` (`user_id`),
-  CONSTRAINT `fk_avatar_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-
-
-DROP TABLE IF EXISTS `comment`;
-
-CREATE TABLE `comment` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `content_id` bigint NOT NULL,
-  `parent_id` bigint NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  KEY `fk_comment_1_idx` (`content_id`,`parent_id`),
-  KEY `fk_comment_2_idx` (`parent_id`),
-  CONSTRAINT `fk_comment_1` FOREIGN KEY (`content_id`) REFERENCES `question_content` (`id`),
-  CONSTRAINT `fk_comment_2` FOREIGN KEY (`parent_id`) REFERENCES `question_content` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
 
 DROP TABLE IF EXISTS `language`;
@@ -60,7 +13,7 @@ CREATE TABLE `language` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `nombre_UNIQUE` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 DROP TABLE IF EXISTS `question`;
@@ -77,7 +30,7 @@ CREATE TABLE `question` (
   UNIQUE KEY `title_UNIQUE` (`title`),
   KEY `fk_question_1_idx` (`language_id`),
   CONSTRAINT `fk_question_1` FOREIGN KEY (`language_id`) REFERENCES `language` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 DROP TABLE IF EXISTS `user`;
@@ -107,7 +60,22 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DROP TABLE IF EXISTS `avatar`;
+
+CREATE TABLE `avatar` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
+  `type` enum('gravatar','custom') NOT NULL DEFAULT 'custom',
+  `avatar` varchar(1024) NOT NULL,
+  `selected` tinyint(1) NOT NULL DEFAULT '1',
+  `added_at` int unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_avatar_1_idx` (`user_id`),
+  CONSTRAINT `fk_avatar_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 DROP TABLE IF EXISTS `user-language`;
@@ -121,9 +89,9 @@ CREATE TABLE `user-language` (
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `fk_usuario - lenguaje_1_idx` (`id_user`),
   KEY `fk_usuario - lenguaje_2_idx` (`id_language`),
-  CONSTRAINT `fk_user - language_1` FOREIGN KEY (`id_language`) REFERENCES `language` (`id`),
-  CONSTRAINT `fk_user - language_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+  CONSTRAINT `fk_user-language_1` FOREIGN KEY (`id_language`) REFERENCES `language` (`id`),
+  CONSTRAINT `fk_user-language_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 DROP TABLE IF EXISTS `question_content`;
@@ -148,7 +116,39 @@ CREATE TABLE `question_content` (
   CONSTRAINT `fk_question_content_2` FOREIGN KEY (`author`) REFERENCES `user` (`id`),
   CONSTRAINT `fk_question_content_3` FOREIGN KEY (`last_activity_by_id`) REFERENCES `user` (`id`),
   CONSTRAINT `fk_question_content_4` FOREIGN KEY (`last_edited_by_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `answer`;
+
+CREATE TABLE `answer` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `validated` tinyint(1) NOT NULL DEFAULT '0',
+  `validated_at` int unsigned DEFAULT NULL,
+  `validated_by_id` bigint DEFAULT NULL,
+  `content_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_answer_1_idx` (`content_id`),
+  KEY `fk_answer_2_idx` (`validated_by_id`),
+  CONSTRAINT `fk_answer_1` FOREIGN KEY (`content_id`) REFERENCES `question_content` (`id`),
+  CONSTRAINT `fk_answer_2` FOREIGN KEY (`validated_by_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `comment`;
+
+CREATE TABLE `comment` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `content_id` bigint NOT NULL,
+  `parent_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_comment_1_idx` (`content_id`,`parent_id`),
+  KEY `fk_comment_2_idx` (`parent_id`),
+  CONSTRAINT `fk_comment_1` FOREIGN KEY (`content_id`) REFERENCES `question_content` (`id`),
+  CONSTRAINT `fk_comment_2` FOREIGN KEY (`parent_id`) REFERENCES `question_content` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 DROP TABLE IF EXISTS `question_close_reason`; 
@@ -159,7 +159,7 @@ CREATE TABLE `question_close_reason` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `reason_UNIQUE` (`reason`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 DROP TABLE IF EXISTS `question_closed`;
@@ -178,7 +178,7 @@ CREATE TABLE `question_closed` (
   CONSTRAINT `fk_question_closed_1` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`),
   CONSTRAINT `fk_question_closed_2` FOREIGN KEY (`closed_by_id`) REFERENCES `user` (`id`),
   CONSTRAINT `fk_question_closed_3` FOREIGN KEY (`closed_reason`) REFERENCES `question_closed` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 DROP TABLE IF EXISTS `vote`;
@@ -195,7 +195,51 @@ CREATE TABLE `vote` (
   CONSTRAINT `fk_vote_1` FOREIGN KEY (`content_id`) REFERENCES `question_content` (`id`),
   CONSTRAINT `fk_vote_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   CONSTRAINT `vote_inrange` CHECK (((`vote` = -(1)) or (`vote` = 1)))
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DELIMITER ;;
+CREATE PROCEDURE `updateUserLanguageReputation`(IN idrow BIGINT)
+BEGIN
+    DECLARE aux INTEGER DEFAULT 1;
+    DECLARE rep INTEGER DEFAULT 0;
+
+    SELECT reputation
+	INTO rep
+    FROM `user-language`
+    WHERE id = idrow;
+    
+    IF rep - 2 >= 1 THEN
+		SET aux = rep - 2;
+	END IF;
+    
+	UPDATE `user-language`
+        SET reputation=aux
+        WHERE id = idrow;
+END ;;
+DELIMITER ;
+
+
+DELIMITER ;;
+CREATE PROCEDURE `updateUserPlatformReputation`(IN userID BIGINT)
+BEGIN
+    DECLARE aux INTEGER DEFAULT 1;
+    DECLARE rep INTEGER DEFAULT 0;
+
+    SELECT reputation
+	INTO rep
+    FROM user
+    WHERE id = userID;
+    
+    IF rep - 2 >= 1 THEN
+		SET aux = rep - 2;
+	END IF;
+    
+	UPDATE user
+        SET reputation=aux
+        WHERE id = userID;
+END ;;
+DELIMITER ;
 
 
 DELIMITER ;;
@@ -205,7 +249,7 @@ BEGIN
     DECLARE finished INTEGER DEFAULT 0;
     DECLARE idUserLanguageRel BIGINT DEFAULT 0;
     
-	DECLARE pointer CURSOR FOR SELECT id FROM `user - language`;
+	DECLARE pointer CURSOR FOR SELECT id FROM `user-language`;
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished = 1;  
 	
     OPEN pointer;
@@ -249,49 +293,6 @@ BEGIN
 END ;;
 DELIMITER ;
 
-
-DELIMITER ;;
-CREATE PROCEDURE `updateUserLanguageReputation`(IN idrow BIGINT)
-BEGIN
-    DECLARE aux INTEGER DEFAULT 1;
-    DECLARE rep INTEGER DEFAULT 0;
-
-    SELECT reputation
-	INTO rep
-    FROM `user - language`
-    WHERE id = idrow;
-    
-    IF rep - 2 >= 1 THEN
-		SET aux = rep - 2;
-	END IF;
-    
-	UPDATE `user - language`
-        SET reputation=aux
-        WHERE id = idrow;
-END ;;
-DELIMITER ;
-
-
-DELIMITER ;;
-CREATE PROCEDURE `updateUserPlatformReputation`(IN userID BIGINT)
-BEGIN
-    DECLARE aux INTEGER DEFAULT 1;
-    DECLARE rep INTEGER DEFAULT 0;
-
-    SELECT reputation
-	INTO rep
-    FROM user
-    WHERE id = userID;
-    
-    IF rep - 2 >= 1 THEN
-		SET aux = rep - 2;
-	END IF;
-    
-	UPDATE user
-        SET reputation=aux
-        WHERE id = userID;
-END ;;
-DELIMITER ;
 
 CREATE EVENT IF NOT EXISTS reduceReputationDaily
 ON SCHEDULE EVERY 5 DAY_HOUR
